7a4159e7c816e0d46106cabbe29a466aa301f9a7
Jonathan Ross <jonathan@jibo.com>
2016-03-04 16:10:19 -0800

    Update to v1.2.0

---
+++ install.js
@@ -0,0 +1,51 @@
+var fs = require('fs-extra');
+var path = require('path');
+var nugget = require('nugget');
+var extract = require('extract-zip');
+
+var downloadUrl = require('./download').url;
+var version = require('./download').version;
+var platform = process.platform;
+var arch = platform === 'win32' ? 'ia32' : process.arch;
+
+
+var target = 'jibo-nlu-js-v' + version + '-' + platform + '-' + arch + '.zip';
+var targetDir = 'jibo-nlu-js';
+downloadUrl += target;
+
+var cacheDir = path.join(__dirname, 'tmp');
+var copyDir = path.join(__dirname, 'parser');
+
+var opts = {
+    target: target,
+    dir: cacheDir,
+    verbose: true,
+    strictSSL: true,
+    resume: true
+};
+try {
+    fs.removeSync(cacheDir);
+    fs.removeSync(copyDir);
+}
+catch(e) {
+    //don't care
+}
+
+fs.mkdirsSync(cacheDir);
+fs.mkdirsSync(copyDir);
+
+nugget(downloadUrl, opts, function (err) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    extract(path.join(cacheDir, target), {dir: cacheDir}, function (err) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        fs.copySync(path.join(cacheDir, targetDir), copyDir, {clobber: true});
+        fs.remove(cacheDir, function() {});
+    });
+});
+
